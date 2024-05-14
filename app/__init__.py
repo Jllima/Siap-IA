27ee081f068a7a8cbf15a5ac7a8930c9ae976676
Jorge <jlimajorge@gmail.com>
2024-05-14 13:55:06 -0300

    chore: inicializando o banco

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
 home_bp = Blueprint('home', __name__, url_prefix='/')
+db = SQLAlchemy()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    db.init_app(app)
     
     from .home.routes import home_bp
     app.register_blueprint(home_bp)
