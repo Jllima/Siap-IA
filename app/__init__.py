90b763d8acd6332d4af594e6966f6931affd22f8
Jorge <jlimajorge@gmail.com>
2024-05-14 13:52:47 -0300

    chore: config sqlite

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask, Blueprint
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 home_bp = Blueprint('home', __name__, url_prefix='/')
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
+    app.config.from_object(config_class)
     
     from .home.routes import home_bp
     app.register_blueprint(home_bp)
